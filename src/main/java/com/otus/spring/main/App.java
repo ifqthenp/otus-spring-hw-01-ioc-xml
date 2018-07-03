c1c35b94a9bf5d30b9e8842960eb4594b2371690
Andrei Bogomja <bogomja@gmail.com>
2018-07-03 17:11:46 +0100

    Add main package

--- src/main/java/com/otus/spring/main/App.java
+++ src/main/java/com/otus/spring/main/App.java
@@ -1,9 +1,20 @@
 package com.otus.spring.main;
 
+import com.otus.spring.service.CsvService;
+import com.otus.spring.service.QuizService;
+import com.otus.spring.service.impl.QuizServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App
 {
     public static void main(String[] args)
     {
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("classpath:spring-context.xml");
+        CsvService csvService = context.getBean(CsvService.class);
 
+        QuizService quizService = new QuizServiceImpl(csvService);
+        quizService.run();
     }
 }
